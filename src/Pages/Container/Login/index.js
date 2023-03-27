7b50c591bc0cc3a63abf819b88f84cdb3933b729
quyet pham <nvan9712@gmail.com>
2023-03-27 20:17:07 +0700

    update forrm login

--- src/Pages/Container/Login/index.js
+++ src/Pages/Container/Login/index.js
@@ -7,42 +7,70 @@ import clientUtils from "../../../utils/client-utils";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const [mail, setMail] = useState();
-  const [pass, setPass] = useState();
-  let navigate = useNavigate();
-  const checkMail = (e) => {
-    setMail(e.target.value);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
   };
-  const checkPass = (e) => {
-    setPass(e.target.value);
-  };
-  const hanldleSubmit = async () => {
+
+  const handleSubmit = async () => {
+    if (isSubmitting) {
+      return;
+    }
+    if (!email) {
+      setError("Please enter your email");
+      return;
+    }
+    if (!password) {
+      setError("Please enter your password");
+      return;
+    }
+    setIsSubmitting(true);
     const bodyParamster = {
-      mail: mail,
-      password: md5(pass),
+      mail: email,
+      password: md5(password),
     };
-    dispatch(getUsers(bodyParamster));
+    dispatch(getUsers(bodyParamster)).then(() => {
+      setIsSubmitting(false);
+    });
   };
+
   const userState = useSelector((store) => store.user);
+
   if (userState.user.code === 200) {
     navigate("/admin");
     window.location.reload(false);
-  } else {
-    
   }
-  useEffect(() => {
-    if (clientUtils.auth) {
-      navigate("/admin");
-    }
-  }, []);
+
   return (
     <div>
-      <h1>login</h1>
-      <label>Mail</label>
-      <input type="text" onChange={checkMail} />
-      <label>Password</label>
-      <input type="text" onChange={checkPass} />
-      <button onClick={hanldleSubmit}>click login</button>
+      <h1>Login</h1>
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </label>
+      {error && <div>{error}</div>}
+      <button onClick={handleSubmit}>Log in</button>
     </div>
   );
 };
